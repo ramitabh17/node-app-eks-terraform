8bdd993cccb3af96f75f7aa00629f535a1e34535
Ramit Amitabh <ramit.amitabh24@gmail.com>
2022-01-31 15:07:56 +0530

    ingress changes

--- k8-resources/service.tf
+++ k8-resources/service.tf
@@ -1,20 +1,34 @@
-resource "kubernetes_service" "nginx" {
+# resource "kubernetes_service" "nginx" {
+#   metadata {
+#     name = "nginx-example"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+#     }
+#     port {
+#       port        = 3000
+#       target_port = 3000
+#     }
+
+#     type = "LoadBalancer"
+#   }
+# }
+
+# output "lb_ip" {
+#   value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
+# }
+
+resource "kubernetes_service" "example" {
   metadata {
-    name = "nginx-example"
+    name = "ingress-service"
   }
   spec {
-    selector = {
-      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
-    }
     port {
-      port        = 3000
+      port = 3000
       target_port = 3000
+      protocol = "TCP"
     }
-
-    type = "LoadBalancer"
+    type = "NodePort"
   }
-}
-
-output "lb_ip" {
-  value = kubernetes_service.nginx.status.0.load_balancer.0.ingress.0.hostname
 }
